41d10fd81190678788126e8f0363c95c24305ae1
Link <fpp.jesus@gmail.com>
2018-05-31 01:35:51 +0100

    Fixed error in logging cubbed items

--- d2bs/kolbot/libs/common/Cubing.js
+++ d2bs/kolbot/libs/common/Cubing.js
@@ -971,13 +971,8 @@ IngredientLoop:
 							
 						case 5: // Crafting System
 							CraftingSystem.update(items[j]);
-							Misc.itemLogger("Cubbed", item, "Cubing System");
-							if (!(item.classid >= 557 && item.classid <= 586)) Misc.logItem("Cubbed", item, "Cubing System"); // Log all but gems
-							break;
-							
-						default:
-							Misc.itemLogger("Cubbed", item, "Cubing System");
-							if (!(item.classid >= 557 && item.classid <= 586)) Misc.logItem("Cubbed", item, "Cubing System"); // Log all but gems
+							Misc.itemLogger("Cubbed", items[j], "Cubing System");
+							if (!(items[j].classid >= 557 && items[j].classid <= 586)) Misc.logItem("Cubbed", items[j], "Cubing System"); // Log all but gems
 							break;
 						}
 					}
